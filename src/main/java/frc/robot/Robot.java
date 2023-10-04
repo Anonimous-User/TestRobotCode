dd06eec850034631751e8bb08a1a0bb6e3a9bee2
anonimous-user <harrisbond1234@gmail.com>
2023-10-04 01:47:47 -0400

    updted stuff, tried to find code for balancing but we might have deleted it already

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,38 +4,32 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
-/** This is a demo program showing how to use Mecanum control with the MecanumDrive class. */
+
 public class Robot extends TimedRobot {
   private static final int kFrontLeftChannel = 2;
   private static final int kRearLeftChannel = 0;
   private static final int kFrontRightChannel = 3;
   private static final int kRearRightChannel = 1;
 
-  private static final int kJoystickChannel = 0;
-
   private MecanumDrive m_robotDrive;
   XboxController m_driverController = new XboxController(0);
 
-  private PWMSparkMax frontLeft = new PWMSparkMax(kFrontLeftChannel);
-  private PWMSparkMax rearLeft = new PWMSparkMax(kRearLeftChannel);
-  private PWMSparkMax frontRight = new PWMSparkMax(kFrontRightChannel);
-  private PWMSparkMax rearRight = new PWMSparkMax(kRearRightChannel);
+  private PWMSparkMax m_frontLeft = new PWMSparkMax(kFrontLeftChannel);
+  private PWMSparkMax m_rearLeft = new PWMSparkMax(kRearLeftChannel);
+  private PWMSparkMax m_frontRight = new PWMSparkMax(kFrontRightChannel);
+  private PWMSparkMax m_rearRight = new PWMSparkMax(kRearRightChannel);
 
   @Override
   public void robotInit() {
 
-    // Invert the right side motors.
-    // You may need to change or remove this to match your robot.
-    frontRight.setInverted(false);
-    rearRight.setInverted(true);
+    m_rearRight.setInverted(true);
 
-    m_robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
+    m_robotDrive = new MecanumDrive(m_frontLeft, m_rearLeft, m_frontRight, m_rearRight);
   }
 
   @Override
@@ -70,15 +64,15 @@ public class Robot extends TimedRobot {
     }
 
     if(leftFront==0&&leftRear==0&&rightFront==0&&rightRear==0){
-      this.frontLeft.stopMotor();
-      this.rearLeft.stopMotor();
-      this.rearRight.stopMotor();
-      this.frontRight.stopMotor();
+      m_frontLeft.stopMotor();
+      m_rearLeft.stopMotor();
+      m_rearRight.stopMotor();
+      m_frontRight.stopMotor();
     } else{
-      frontLeft.set(leftFront*.8);
-      rearLeft.set(leftRear*.8);
-      rearRight.set(rightRear*.8);
-      frontRight.set(rightFront*.8);
+      m_frontLeft.set(leftFront*.8);
+      m_rearLeft.set(leftRear*.8);
+      m_rearRight.set(rightRear*.8);
+      m_frontRight.set(rightFront*.8);
     }
   }
 }
